56c7b486f14bc7ce9109d8400b62fc2aeabddc59
Enochteo <enochowoade@gmail.com>
2025-08-07 23:52:04 -0500

    fix date sorting

--- data-script.py
+++ data-script.py
@@ -1,33 +1,32 @@
 import os
 import csv
+from datetime import datetime
 
 DATA_DIR = "./data"
 NEW_DATA = "./valuable-data.csv"
 
+rows = []
+for filename in os.listdir(DATA_DIR):
+    with open(f"{DATA_DIR}/{filename}", mode="r") as input_file:
+        reader = csv.reader(input_file)
+
+        next(reader)
+        for row in reader:
+            product = row[0]
+            raw_price = row[1]
+            quantity = row[2]
+            date = row[3]
+            region = row[4]
+            if product == "pink morsel":
+                price = float(raw_price[1:])
+                sales = price * int(quantity)
+                rows.append([sales, date, region])
+rows.sort(key=lambda x: datetime.strptime(x[1], "%Y-%m-%d"))
+
 with open(NEW_DATA, mode='w') as output_file:
     writer = csv.writer(output_file)
+    writer.writerow(["sales", "date", "region"])
+    for row in rows:
+        writer.writerow(row)
 
-    header = ["sales", "date", "region"]
-    writer.writerow(header)
-
-    for filename in os.listdir(DATA_DIR):
-        with open(f"{DATA_DIR}/{filename}", mode="r") as input_file:
-            reader = csv.reader(input_file)
-
-            row_index = 0
-            for row in reader:
-                if row_index > 0:
-                    product = row[0]
-                    raw_price = row[1]
-                    quantity = row[2]
-                    date = row[3]
-                    region = row[4]
-                    if product == "pink morsel":
-                        price = float(raw_price[1:])
-                        sales = price * int(quantity)
-
-                        output_row = [sales, date, region]
-                        writer.writerow(output_row)
-                row_index += 1
 print('done')
-
